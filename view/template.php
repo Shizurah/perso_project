faf0ea9b1d74c3effb94ddbf89bb25c521d99387
Shizurah <eloise-martin@live.fr>
2018-07-30 17:10:19 +0200

    Création d'un template de page

---
+++ view/template.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf-8"/>
+        
+        <title><?= $title ?></title>
+        <link rel="stylesheet" href="<?= $href ?>">
+    </head>
+
+    <body>
+        <?= $header ?>
+        <?= $section ?>
+        <?= $footer ?>
+    </body>
+
+</html>
