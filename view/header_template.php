651c20c67d56b7667f87d5c3688f801cd3ec6bc8
Shizurah <eloise-martin@live.fr>
2018-07-31 17:19:16 +0200

    Création d'un template pour la partie <header> des pages du site

---
+++ view/header_template.php
@@ -0,0 +1,13 @@
+<header>
+    <h2><?= $h2Header ?></h2>
+
+    <nav>
+        <a href="index.php">Actus</a>
+        <a href="index.php?action=weLoved">On a aimé</a>
+        <a href="index.php?action=tvShows">Séries TV</a>
+        <a href="index.php?action=mySpace">Mon espace</a>
+
+        <a href="index.php?action=connexion">Connexion</a>
+        <a href="index.php?action=deconnexion">Déconnexion</a>
+    </nav>
+</header>
