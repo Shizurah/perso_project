c70f170b03514543d60038ff3e35b52c58e3da8f
Shizurah <eloise-martin@live.fr>
2018-08-02 15:28:33 +0200

    Refactoring

--- controllers/posts_controller.php
+++ controllers/posts_controller.php
@@ -1,37 +1,14 @@
 <?php
 
-function startSession() {
-    if (!isset($_SESSION)) {
-        session_start();
-    }
-}
-
 function homePage() {
-    startSession();
     require_once('view/news_view.php');
 }
 
 function weLovedPage() {
-    startSession();
     require_once('view/weLoved_view.php');
 }
 
 function tvShowsPage() {
-    startSession();
     require_once('view/tvShows_view.php');
 }
 
-function mySpacePage() {
-    startSession();
-    require_once('view/mySpace_view.php');
-}
-
-function connexionPage() {
-    startSession();
-    require_once('view/connexion_view.php');
-}
-
-function contactPage() {
-    startSession();
-    require_once('view/contact_view.php');
-}
--- controllers/users_controller.php
+++ controllers/users_controller.php
@@ -1,9 +1,41 @@
 <?php
 
-function addNewUser ($pseudo, $pass, $email) {
+function mySpacePage($pseudo) {
+    require_once('view/mySpace_view.php');
+}
+
+function connexionPage() {
+    require_once('view/connexion_view.php');
+}
+
+function contactPage() {
+    require_once('view/contact_view.php');
+}
+
+function userRegistration($pseudo, $pass, $email) {
     $pass = password_hash($pass, PASSWORD_DEFAULT);
 
     $usersManager = new UsersManager();
-
     $usersManager->addUser($pseudo, $pass, $email);
+}
+
+
+function userConnexion($pseudo, $pass) {
+    $pass = password_hash($pass, PASSWORD_DEFAULT);
+
+    $usersManager = new UsersManager();
+    $user = $usersManager->getUser($pseudo, $pass);
+
+    if (!empty($user)) {
+        session_start();
+
+        $_SESSION['id'] = $user->id();
+        $_SESSION['pseudo'] = $pseudo;
+
+        mySpacePage();
+    } 
+    
+    else {
+        connexionPage();
+    }
 }
