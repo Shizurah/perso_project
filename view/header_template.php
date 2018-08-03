b13b44392d2052ea4b2cfb149e8281160dd740a5
Shizurah <eloise-martin@live.fr>
2018-08-03 17:47:12 +0200

    Gestion de la session pour adaptation du menu quand connecté/déconnecté

--- view/header_template.php
+++ view/header_template.php
@@ -6,10 +6,15 @@
         <a href="index.php">Actus</a>
         <a href="index.php?action=weLoved">On a aimé</a>
         <a href="index.php?action=tvShows">Séries TV</a>
-        <a href="index.php?action=mySpace">Mon espace</a>
 
-        <a href="index.php?action=connexionPage">Connexion</a>
-        <a href="index.php?action=deconnexion">Déconnexion</a>
+        <?php
+            if (isset($_SESSION)) {
+                echo '<a href="index.php?action=mySpace">Mon espace</a> 
+                      <a href="index.php?action=deconnexion">Déconnexion</a>';
+            } else {
+                echo '<a href="index.php?action=connexionPage">Connexion</a>';
+            }
+        ?>      
     </nav>
 
 </header>
