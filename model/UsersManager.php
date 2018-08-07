0f78aae7604595363cbf101a7c2381e2f8d00ab2
Shizurah <eloise-martin@live.fr>
2018-08-07 14:35:28 +0200

    Ajout d'une méthode de récupération du mot de passe associé au pseudo renseigné par l'utilisateur

--- model/UsersManager.php
+++ model/UsersManager.php
@@ -18,24 +18,33 @@ class UsersManager {
         $req->execute();
     }
 
+    public function getPass($pseudo) {
+        $req = $this->_db->prepare('SELECT pass FROM users WHERE pseudo = :pseudo');
+
+        $req->bindValue('pseudo', $pseudo, PDO::PARAM_STR);
+        $req->execute();
+
+        $userPass = $req->fetch();
+        return $userPass['pass'];
+    }
+
     public function getUser($pseudo, $pass) {
-        $req = $this->_db->prepare('SELECT id, pseudo, pass, email, avatar, userStatus FROM users WHERE pseudo = :pseudo AND pass = :pass');
+        $req = $this->_db->prepare('SELECT id, pseudo, email, avatar, userStatus FROM users WHERE pseudo = :pseudo AND pass = :pass');
 
         $req->bindValue('pseudo', $pseudo, PDO::PARAM_STR);
         $req->bindValue('pass', $pass, PDO::PARAM_STR);
 
         $req->execute();
 
-        $infosUser = $req->fetch();
+        $infosUser = $req->fetch(PDO::FETCH_ASSOC);
 
         if (!empty($infosUser)) {
             return new User($infosUser);
-        } else {
-            throw new Exception('Mauvais identifiant ou mot de passe');
-        }
+        } 
     }
 
     public function update() {
 
     }
 }
+
