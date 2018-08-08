10cfb20c804a46231aa3bd651533493d8a08bd33
Shizurah <eloise-martin@live.fr>
2018-08-08 16:38:00 +0200

    Ajout d'un lien 'Administration' au menu de navigation pour les administrateurs du site

--- view/header_template.php
+++ view/header_template.php
@@ -3,12 +3,18 @@
     <h2><?= $h2Header ?></h2>
 
     <nav>
+        <?php 
+            if (isset($_SESSION['userStatus']) && $_SESSION['userStatus'] == 'admin') {
+                echo '<a href="index.php?action=administration">Administration</a>';
+            }
+        ?>
+
         <a href="index.php">Actus</a>
         <a href="index.php?action=weLoved">On a aimé</a>
         <a href="index.php?action=tvShows">Séries TV</a>
 
         <?php
-            if (isset($_SESSION)) {
+            if (isset($_SESSION['pseudo'])) {
                 echo '<a href="index.php?action=mySpace">Mon espace</a> 
                       <a href="index.php?action=deconnexion">Déconnexion</a>';
             } else {
