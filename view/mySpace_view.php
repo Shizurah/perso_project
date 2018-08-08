9ce11c0ef47b3cc05d24d066281971cc3e3554fb
Shizurah <eloise-martin@live.fr>
2018-08-08 16:47:35 +0200

    Gestion de l'affichage de l'avatar + ajout des formulaires de modification d'avatar et d'informations utilisateur

--- view/mySpace_view.php
+++ view/mySpace_view.php
@@ -14,14 +14,64 @@
 <!-- SECTION -->
 <?php ob_start(); ?>
 
-<p>Ici votre espace</p>
+<p><img src="public/members/avatars/<?= $_SESSION['avatar'] ?>" alt="avatar"></p>
+<h2>Bonjour <?= $_SESSION['pseudo'] ?> </h2>
 
+<br/><br/>
+
+<h3>Modifier votre avatar</h3>
+
+<form action="index.php?action=avatar" method="post" enctype="multipart/form-data">
+    <p>
+        <input type="hidden" name="max-file-size" value="204800"> <!-- valeur en octets -->
+        <input type="file" name="avatar">
+    </p>
+    
+    <input type="submit" name="submit-avatar" value="Importer votre avatar">
+</form>
+<br/><br/>
+<!-- -->
+
+<h3>Modifier vos informations</h3>
+
+<form action="">
+    <p>
+        <label for="pseudo">Modifier votre pseudo :</label><br/>
+        <input type="text" name="pseudo" id="pseudo">
+    </p>
+    
+    <input type="submit" value="Modifier pseudo">
+</form>
+
+<form action="">
+    <p>
+        <label for="pass1">Modifier votre mot de passe :</label><br/>
+        <input type="password" name="pass1" id="pass1">
+    </p>
+
+    <p>
+        <label for="pass2">Confirmer mot de passe :</label><br/>
+        <input type="password" name="pass2" id="pass2">
+    </p>
+    
+    <input type="submit" value="Modifier mot de passe">
+</form>
+
+<form action="">
+    <p>
+        <label for="email">Modifier votre e-mail :</label><br/>
+        <input type="email" name="email" id="email">
+    </p>
+
+    <input type="submit" value="Modifier e-mail">
+</form>
+<br/><br/>
 <?php $section = ob_get_clean(); ?>
 
 
 <!-- FOOTER -->
 <?php ob_start(); ?>
-
+<br/><br/>
 <p>Ici des infos en bas de page</p>
 <a href="index.php?action=contact">Contact</a>
 
