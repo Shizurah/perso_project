ba81ff0020c0af0afbce099b7dcbdb5030097b5a
Shizurah <eloise-martin@live.fr>
2018-08-13 17:11:38 +0200

    Définition d'une fonction pour ajout de nouveaux articles en bdd

--- controllers/posts_controller.php
+++ controllers/posts_controller.php
@@ -13,6 +13,39 @@ function tvShowsPage() {
 }
 
 function tinyMcePage() {
-    require_once('view/tinyMce_view.php');
+
+    if (isset($_SESSION['userStatus']) && $_SESSION['userStatus'] == 'admin') {
+        require_once('view/tinyMce_view.php');
+    } 
+
+    else {
+        echo 'fail';
+        // throw new Exception('Vous n\'êtes pas autorisé à vous rendre sur cette page');
+    }
 }
 
+
+function addNewPost($title, $category, $content) {
+
+    if (isset($_SESSION['userStatus']) && $_SESSION['userStatus'] == 'admin') {
+
+        $postCategory = '';
+
+        if ($category == 'news') {
+            $postCategory = 'Actus';
+        }
+        elseif ($category == "weLoved") {
+            $postCategory = 'On a aimé';
+        }
+
+        $postsManager = new PostsManager();
+        $newPostId = $postsManager->addPost($title, $postCategory, $content);
+
+        require_once('view/tinyMce_view.php');
+    }
+
+    else {
+        echo 'Vous n\'êtes pas autorisé à effectuer cette action';
+    }
+    
+}
