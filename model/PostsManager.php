3e197da23b9a751bd8cde1d7bad7f675df979b0e
Shizurah <eloise-martin@live.fr>
2018-08-13 18:40:46 +0200

    Définition d'une méthode de récupération d'un article en bdd (à partir de son id)

--- model/PostsManager.php
+++ model/PostsManager.php
@@ -2,20 +2,19 @@
 
 class PostsManager {
 
-    private $db;
+    private $_db;
 
     public function __construct() {
         $this->_db = DbManager::dbConnect();
     }
 
     public function addPost($title, $category, $content) {
-        $req = $this->_db->prepare('INSERT INTO posts(title, category, content, datePost) VALUES(:title, :category, :content, NOW())');
+        $req = $this->_db->prepare('INSERT INTO posts(title, category, content, postDate) VALUES(:title, :category, :content, NOW())');
 
         $req->bindParam('title', $title, PDO::PARAM_STR);
         $req->bindParam('category', $category, PDO::PARAM_STR);
         $req->bindParam('content', $content, PDO::PARAM_STR);
         // $req->bindParam('poster', $poster, PDO::PARAM_STR);
-
         $req->execute();
 
         return $this->_db->lastInsertId();
@@ -25,8 +24,14 @@ class PostsManager {
 
     }
 
-    public function getPost() {
+    public function getPost($id) {
+        $req = $this->_db->prepare('SELECT id, title, category, content, DATE_FORMAT(postDate, \'%d/%m/%Y\') AS postDate_fr FROM posts WHERE id = :id');
 
+        $req->bindParam('id', $id, PDO::PARAM_INT);
+        $req->execute();
+
+        $data = $req->fetch();
+        return new Post($data);
     }
 
     public function updatePost() {
