4472e3c5f470a39ea5c5978b470145cfe4f6ba5f
Shizurah <eloise-martin@live.fr>
2018-08-14 15:37:37 +0200

    Création d'un fichier pour menu de l'espace d'administration

---
+++ view/asideForAdministration_template.php
@@ -0,0 +1,8 @@
+<aside>
+    <a href="index.php?action=infosSite">Infos site</a> <!-- nb utilisateurs inscrits, commentaires signalés, msg envoyés etc -->
+    <a href="index.php?action=allPostsList">Articles</a> <!-- liens de modification et de suppression pour chaque article -->
+    <a href="index.php?action=postWriting">Rédiger un article</a>
+    <a href="index.php?action=">Commentaires utilisateurs</a> <!-- liens ignorer et supprimer pour chaque commentaire -->
+    <a href="index.php">Retour au site</a>
+    <a href="index.php?action=deconnexion">Déconnexion</a>
+</aside>
