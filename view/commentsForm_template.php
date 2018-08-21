30863f20330c986e749dbebff9f23aad6edc32ec
Shizurah <eloise-martin@live.fr>
2018-08-21 15:44:46 +0200

    Création d'un template pour le formulaire d'ajout/modification d'un commentaire

---
+++ view/commentsForm_template.php
@@ -0,0 +1,9 @@
+<form id="form" action="<?= $formActionAttribute ?>" method="post">
+    <img src="public/members/avatars/<?= $_SESSION['avatar'] ?>" width="50" height="50" alt="avatar">
+
+    <textarea name="comment-text" id="comment-text" placeholder="<?= $placeholderAttribute ?>" cols="40" rows="2"><?= $textareaContent ?></textarea>
+        
+    <br/><br/>
+
+    <input type="submit" value="Publier">
+</form>
