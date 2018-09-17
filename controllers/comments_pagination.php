518415dcd477f8114c8b7986fd13caef5874c48a
Ozarah <eloise-martin@live.fr>
2018-09-17 15:41:33 +0200

    Utilisation d'un tableau d'objets pour construire la réponse renvoyée par le serveur + modification attributs href des liens de la réponse renvoyée

--- controllers/comments_pagination.php
+++ controllers/comments_pagination.php
@@ -1,79 +1,80 @@
 <?php
 
 function displayComments($postId, $page, $commentsPerPage) {
-    $comment = '';
     $commentsManager = new CommentsManager();
     $nbOfComments = $commentsManager->countNumberOfComments($postId);
 
+    $comments = '';
     $nbOfPages = ceil($nbOfComments / $commentsPerPage);
     $firstCommentToDisplay = ($page - 1) * $commentsPerPage;
 
-    $req = $commentsManager->getCommentsToDisplay($postId, $firstCommentToDisplay, $commentsPerPage);
+    $commentsAndUsersInfos = $commentsManager->getCommentsToDisplay($postId, $firstCommentToDisplay, $commentsPerPage);
 
-    while ($data = $req->fetch()) {
+    // while ($comment = $req->fetch()) 
+    foreach ($commentsAndUsersInfos as $comment) {
 
-        $comment .= 
-        '<div class="comments" id="comment' .$data['comment_id']. '"> 
+        $comments .= 
+        '<div class="comments" id="comment' .$comment->comment_id(). '"> 
 
             <div class="author-and-content">
                 <div>
-                    <img class="user-avatar-for-comments" src="public/members/avatars/' .$data['author_avatar']. '" alt="avatar membre"/>
+                    <img class="user-avatar-for-comments" src="public/members/avatars/' .$comment->author_avatar(). '" alt="avatar membre"/>
                 </div>
                 
                 <div>
                     <span class="authors">'
-                        .$data['author_pseudo'].
+                        .$comment->author_pseudo().
                     '</span>
                     
-                    <span id="' .$data['comment_id']. '">'
-                        .$data['comment_content'].
+                    <span id="' .$comment->comment_id(). '">'
+                        .$comment->comment_content().
                     '</span>                        
                 </div>
             </div>
 
             <div class="comments-date-and-actions">
-                <i>' .$data['comment_date_fr']. '</i>';
+                <i>' .$comment->comment_date_fr(). '</i>';
     
 
         if (isset($_SESSION['pseudo']) && isset($_SESSION['userStatus'])) {
 
             // 1. possibilité de modifier/supprimer son commentaire :
-            if ($_SESSION['id'] == $data['author_id']) {
-                $comment .=
-                    ' <a class="updating-comment-btn" href="' .$data['comment_id']. '">
+            if ($_SESSION['id'] == $comment->author_id()) {
+                $comments .=
+                    ' <a class="updating-comment-btn" href="' .$comment->comment_id(). '">
                             Modifier
                     </a> - 
 
-                    <a class="deleting-comment-btn" href="' .$data['comment_id']. '" 
+                    <a class="deleting-comment-btn" href="' .$comment->comment_id(). '" 
                     onclick="return confirm(\'Êtes-vous sûr de vouloir supprimer votre commentaire ?\')">
                             Supprimer
                     </a>';   
             }
 
-            // 2. possibilité de signaler les commentaires :
+            // 2. possibilité de signaler les commentairess :
             else {
-                $comment .=
-                    '<a href="index.php?action=commentReporting&commentId=' .$data['comment_id']. '&postId=' .$data['post_id']. '"
+                $comments .=
+                    '<a class="reporting-comment-btn" href="' .$comment->comment_id(). '"
                         onclick="return confirm(\'Êtes-vous sûr de vouloir signaler ce commentaire ?\')">
                             Signaler
                     </a>';
             }
 
             // 3. Pour les admin, possibilité de supprimer chacun des commentaires directement depuis la page :
-            if ($_SESSION['userStatus'] == 'admin' && $_SESSION['id'] != $data['author_id']) {
+            if ($_SESSION['userStatus'] == 'admin' && $_SESSION['id'] != $comment->author_id()) {
 
-                $comment .= 
-                    ' - <a href="index.php?action=commentDeleted&commentId=' .$data['comment_id']. '&postId=' .$data['post_id']. '"
+                $comments .= 
+                    ' - <a class="deleting-comment-btn" href="' .$comment->comment_id(). '"
                             onclick="return confirm(\'Êtes-vous sûr de vouloir supprimer ce commentaire ?\')">
                                 Supprimer
                         </a>';
             }
         }
 
-        $comment .= '</div></div>';              
+        $comments .= '</div></div>';              
     }
 
-    echo $comment;  
+    echo $comments;  
 }
 
 
