f8c88e3c7d2834317b5b44c8d2a601aa6402237e
Ozarah <eloise-martin@live.fr>
2018-09-21 18:00:03 +0200

    Ajout d'un champ pour rechercher une série d'après mots-clé et lien pour remonter haut de page

--- view/tvShows_view.php
+++ view/tvShows_view.php
@@ -8,7 +8,8 @@
     ob_start(); 
 
     echo '<div id="main-wrap" class="container">'; // --> DEBUT MAIN-WRAP
-    $h1Header = '<img id="banner-img" src="public/images/logo2.png" alt="logo"/>';
+    $h1Header = '';
+    // <img id="banner-img" src="public/images/logo2.png" alt="logo"/>
     require('header_template.php'); 
 
     $header = ob_get_clean(); 
@@ -18,18 +19,32 @@
 <!-- SECTION -->
 <?php ob_start(); ?>
 
-    <h3>SERIES DU MOMENT</h3>
+    <h3>RECHERCHER DES SERIES</h3>
     <hr class="title-separations"/>
-    <div id="tv-shows-container"></div>
 
+    <form>
+        <p id="search-container">
+            <input class="form-control" id="search-input" type="text" placeholder="Recherche...">
+            <button type="submit"><img src="public/images/search_icon.png" alt="icône de recherche"></button> 
+        </p>
+    </form>
+    
+    <br/>
+    
+    <h3>SERIES POPULAIRES</h3>
+    <hr class="title-separations"/>
+
+    <div id="tv-shows-container">
+        <a id="scroll-up-arrow" href="#nav"><img src="public/images/scroll_up_arrow.png" alt="flèche retour haut de page"></a>
+    </div>
+
+    
     </div> <!-- FIN MAIN-WRAP -->
 <?php $section = ob_get_clean(); ?>
 
 
 <!-- FOOTER -->
 <?php 
-    // ob_start(); 
-    // require_once('footer_template.php');
     $footer = '';
 
     // SCRIPTS JS :
