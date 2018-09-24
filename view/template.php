7b624debe5b47b986fec3e9b040545a3ee0bd767
Ozarah <eloise-martin@live.fr>
2018-09-24 10:13:13 +0200

    Ré-organisation fichier + ajout commentaires

--- view/template.php
+++ view/template.php
@@ -8,39 +8,35 @@
         <title><?= $title ?></title>
 
         <!-- CSS/BOOTSTRAP -->
-        <!-- <link href="assets/bootstrap/css/bootstrap.css" rel="stylesheet"> -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <link rel="stylesheet" href="assets/bootstrap-select-1.13.2/dist/css/bootstrap-select.css">
         <link href="assets/css/style.css" rel="stylesheet">
+        
         <?= $other ?>
+
         <!-- GOOGLE FONTS -->
         <link href="https://fonts.googleapis.com/css?family=Cabin+Sketch" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Monoton|Permanent+Marker" rel="stylesheet"> 
-        <!-- JQUERY -->
-        
     </head>
 
     <body>
-        <!-- <div id="main-wrap" class="container"> -->
-            <?= $header ?>
+        <?= $header ?>
 
-            <section>
-                <?= $section ?>
-            </section>
+        <section>
+            <?= $section ?>
+        </section>
 
-            <?= $footer ?>
-
-            
-        <!-- </div> -->
+        <?= $footer ?>
         
-
-        <!-- scripts -->
+        <!-- SCRIPTS-->
+        <!-- jquery : -->
         <script src="assets/js/jquery-3.3.1.js"></script>
+        <!-- fichiers nécessaires au plugin 'BOOTSTRAP SELECT' : -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="assets/bootstrap/js/bootstrap.js"></script>
-
         <script src="assets/bootstrap-select-1.13.2/dist/js/bootstrap-select.min.js"></script>
         <script src="assets/bootstrap-select-1.13.2/dist/js/i18n/defaults-fr_FR.min.js"></script>
+        
         <?= $scripts ?>
     </body>
 
