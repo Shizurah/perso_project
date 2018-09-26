c130b98e558fda814faf0eec202989a47bc0b7a4
Ozarah <eloise-martin@live.fr>
2018-09-26 18:14:18 +0200

    Réorganisation code

--- view/connexion_view.php
+++ view/connexion_view.php
@@ -17,74 +17,36 @@
 
 <!-- SECTION -->
 <?php ob_start(); ?>
-    
-    <div class="row">
 
-        <div class="col-lg-6">
-            <p>Je me connecte :</p>
+<!-- <div id="connexion-and-registration-forms-container" class="row"> -->
+    <div id="connexion-and-registration-forms-container">
 
-            <p id="errorMsg"></p> <!-- message si erreur rencontrée -->
+        <h4 id="form-title">Connexion</h4>
 
-            <form id="connexion-form" action="index.php?action=connexion" method="post">
-                <p>
-                    <label for="pseudo">Pseudo :</label>
-                    <input type="text" name="pseudo" id="pseudo" required>
-                </p>
-                    
-                <p>
-                    <label for="password">Mot de passe :</label>
-                    <input type="password" name="password" id="password" required>
-                </p>
+        <p id="errorMsg"></p> <!-- message si erreur rencontrée -->
 
-                <p>
-                    <input type="submit" value="Connexion">
-                </p>
-            </form>
-        </div>
-
-        <div class="col-lg-6">
-            <p>Pas encore de compte ?</p>
-
-            <form action="index.php?action=registration" method="post">
-                <p>
-                    <label for="pseudo">Pseudo :</label>
-                    <input type="text" name="pseudo" id="pseudo" required>
-
-                    <?php 
-                        if (isset($errorPseudo)) {
-                            echo $errorPseudo;
-                        }
-                    ?>
-                </p>
-
-                <p>
-                    <label for="email">E-mail :</label>
-                    <input type="email" name="email" id="email" required>
-                </p>
+        <form id="connexion-form" action="index.php?action=connexion" method="post">
+            <p>
                 
-                <p>
-                    <label for="password1">Mot de passe :</label>
-                    <input type="password" name="password1" id="password1" required>
-                    
-                    <?php 
-                        if (isset($errorPass)) {
-                            echo $errorPass;
-                        }
-                    ?>
-                </p>
+                <input type="text" name="pseudo" id="pseudo" required><br/>
+                <label for="pseudo">Pseudo</label>
+            </p>
+                
+            <p>
+                
+                <input type="password" name="password" id="password" required><br/>
+                <label for="password">Mot de passe</label>
+            </p>
 
-                <p>
-                    <label for="password2">Confirmation du mot de passe :</label>
-                    <input type="password" name="password2" id="password2" required>
-                </p>
+            
+            <input type="submit" value="Se connecter">
+            
+        </form>
 
-                <p>
-                    <input type="submit" value="Créer mon compte">
-                </p>
-            </form>
-        </div>
-        
+        <a href="index.php?action=registrationPage">Pas encore de compte ?</a> 
     </div>
+<!-- </div> -->
+      
 
 </div> <!-- FIN MAIN-WRAP -->
 <?php $section = ob_get_clean(); ?>
