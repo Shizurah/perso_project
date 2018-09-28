736203f744b586452de87e41d3194261acc0d575
Ozarah <eloise-martin@live.fr>
2018-09-28 19:05:11 +0200

    utilisation du fichier template.php

--- view/tinyMce_view.php
+++ view/tinyMce_view.php
@@ -1,82 +1,103 @@
-<!DOCTYPE html>
-<html>
+<?php 
+    $title = 'Rédiger un article'; 
+    $other = '<link href="assets/css/administration_style.css" rel="stylesheet">';
+?>
 
-    <head>
-        <meta charset="utf-8"/>
-        <title>Rédiger article</title>
-        <!-- BOOTSTRAP -->
-        <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+<!-- TinyMCE : -->
+<script src='https://cloud.tinymce.com/stable/tinymce.min.js'></script>
+<script>
+    tinymce.init({
+        selector: '#postContent',
+        plugins : 'image, link',
+        width: '80%',
+        height: '450px'
+    });
+</script>
 
-        <!-- TinyMCE : -->
-        <script src='https://cloud.tinymce.com/stable/tinymce.min.js'></script>
-        <script>
-            tinymce.init({
-                selector: '#postContent',
-                plugins : 'image, link'
-            });
-        </script>
-    </head>
+<!-- HEADER -->
+<?php 
+    ob_start(); 
+    $h1Header = '';
+    // require('templates/header_template.php'); 
 
-    <body>
+    $header = ob_get_clean(); 
+?>
 
-        <div class="container">
-            
-            <?php 
-                $h1Header = '';
-                require('header_template.php');
-                require('asideForAdministration_template.php'); 
-            ?>
 
-            <br/>
+<!-- SECTION -->
+<?php ob_start(); ?>
 
-            <?php
-                if (!isset($_GET['postId'])) {
-                    echo '<h3>Rédiger un article</h3>';
-                } 
-                else {
-                    echo '<h3>Modifier l\'article</h3>';
+    <!-- <h2>Gestion du site</h2> -->
+    <?php require_once('templates/asideForAdministration_template.php'); ?>
+
+    <div id="administration-main-content">
+
+        <?php
+            if (!isset($_GET['postId'])) {
+                echo '<h3>Rédiger un article</h3>';
+            } 
+            else {
+                echo '<h3>Modifier l\'article</h3>';
+            }
+    
+            if (isset($_GET['action'])) {
+
+                if ($_GET['action'] == 'postWritten') {
+                    echo '<p>Votre article a bien été publié ! <a href="index.php?action=post_and_comments&postId=' . $newPostId . '">Voir l\'article</a></p>';
                 }
-        
-                if (isset($_GET['action'])) {
-
-                    if ($_GET['action'] == 'postWritten') {
-                        echo '<p>Votre article a bien été publié ! <a href="index.php?action=post_and_comments&postId=' . $newPostId . '">Voir l\'article</a></p>';
-                    }
-                    elseif ($_GET['action'] == 'postUpdated' && isset($_GET['postId'])) {
-                        echo '<p>Votre article a bien été modifié ! <a href="index.php?action=post_and_comments&postId=' . $_GET['postId'] . '">Voir l\'article</a></p>';
-                    } 
+                elseif ($_GET['action'] == 'postUpdated' && isset($_GET['postId'])) {
+                    echo '<p>Votre article a bien été modifié ! <a href="index.php?action=post_and_comments&postId=' . $_GET['postId'] . '">Voir l\'article</a></p>';
+                } 
 
 
-                    // FORMULAIRE (fichier postForm_template.php) :
-                    if ($_GET['action'] == 'postUpdating' && isset($_GET['postId'])) {
-                        $formActionAttribute = 'index.php?action=postUpdated&postId=' . $post->id();
-                        $titleValue = $post->title();
+                // FORMULAIRE (fichier postForm_template.php) :
+                if ($_GET['action'] == 'postUpdating' && isset($_GET['postId'])) {
+                    $formActionAttribute = 'index.php?action=postUpdated&postId=' . $post->id();
+                    $titleValue = $post->title();
 
-                        if ($post->category() == 'news') {
-                            $isNewsCategoryChecked = 'checked';
-                            $isNextReleasesCategoryChecked = NULL;
-                        }
-                        elseif ($post->category() == 'next_releases') {
-                            $isNextReleasesCategoryChecked = 'checked';
-                            $isNewsCategoryChecked = NULL;
-                        }
-                        
-                        $postContent = $post->content();
-                    }
-                    else {
-                        $formActionAttribute = 'index.php?action=postWritten';
-                        $titleValue = NULL;
-                        $isNewsCategoryChecked = NULL;
+                    if ($post->category() == 'news') {
+                        $isNewsCategoryChecked = 'checked';
                         $isNextReleasesCategoryChecked = NULL;
-                        $postContent = NULL;
                     }
+                    elseif ($post->category() == 'next_releases') {
+                        $isNextReleasesCategoryChecked = 'checked';
+                        $isNewsCategoryChecked = NULL;
+                    }
+                    
+                    $postContent = $post->content();
+                }
+                else {
+                    $formActionAttribute = 'index.php?action=postWritten';
+                    $titleValue = NULL;
+                    $isNewsCategoryChecked = NULL;
+                    $isNextReleasesCategoryChecked = NULL;
+                    $postContent = NULL;
+                }
 
-                    require_once('postsForm_template.php');
-                }         
-            ?>
-        </div>
+                require_once('view/templates/postsForm_template.php');
+            }         
+        ?>
+    </div>
 
-        <script src="assets/js/file.js"></script>
-    </body>
+<?php $section = ob_get_clean(); ?>
 
-</html>
+
+<!-- FOOTER -->
+<?php 
+    ob_start(); 
+    $footer = ob_get_clean(); 
+
+    // SCRIPTS JS :
+    ob_start();
+?>
+   
+    <script src="assets/js/file.js"></script>
+
+<?php
+    $scripts = ob_get_clean();
+    require_once('templates/template.php'); 
+?>
+
+
+
+
