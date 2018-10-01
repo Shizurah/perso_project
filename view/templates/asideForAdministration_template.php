84ab9f58014a5462af95e646916b389bfb4ef938
Ozarah <eloise-martin@live.fr>
2018-10-01 09:52:32 +0200

    suppression commentaires

--- view/templates/asideForAdministration_template.php
+++ view/templates/asideForAdministration_template.php
@@ -1,12 +1,14 @@
 <aside>
-    <h2>Gestion du site</h2>
+    <div id="administration-aside-content">
+        <h2>Gestion du site</h2>
 
-    <a href="index.php?action=administration">Infos site</a> <!-- nb utilisateurs inscrits, commentaires signalés, msg envoyés etc -->
-    <a href="index.php?action=allPostsList">Articles</a> <!-- liens de modification et de suppression pour chaque article -->
-    <a href="index.php?action=postWriting">Rédiger un article</a>
-    <a href="index.php?action=allPostsList">Modifier un article</a>
-    <a href="index.php?action=reportedComments">Modération des commentaires</a> <!-- liens ignorer et supprimer pour chaque commentaire -->
-    <a href="index.php">Retour au site</a>
-    <a href="index.php?action=deconnexion">Déconnexion</a>
+        <a href="index.php?action=administration">Infos site</a> 
+        <a href="index.php?action=allPostsList">Articles</a>
+        <a href="index.php?action=postWriting">Rédiger un article</a>
+        <a href="index.php?action=allPostsList">Modifier un article</a>
+        <a href="index.php?action=reportedComments">Modération des commentaires</a>
+        <a href="index.php">Retour au site</a>
+        <a href="index.php?action=deconnexion">Déconnexion</a>
+    </div>
 </aside>
 
