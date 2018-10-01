2f2cc6fa4b0d67e012ff95101b8c885d4c9dbc61
Ozarah <eloise-martin@live.fr>
2018-10-01 14:59:55 +0200

    stylisation page de la liste des posts pour modification

--- assets/js/file.js
+++ assets/js/file.js
@@ -1,6 +1,17 @@
 $('#banner').css('display', 'none');
-$('section').css('margin-top', '0');
+$('section').css('margin-top', '0px');
 $('footer').css('background-color', 'white');
 $('footer a').css('color', 'black');
 
 // $('#error-msg-container')
+
+var height = $(window).height();
+$('section').css('height', height);
+
+
+// pages d'administration : modification et suppression des commentaires
+$('.link-for-post-action').hover(function() {
+    $(this).find('img').css('visibility', 'visible');
+}, function() {
+    $(this).find('img').css('visibility', 'hidden');
+});
