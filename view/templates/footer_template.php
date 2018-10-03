db06f981f76d49ce6e356c47e91b8cd6c969780b
Ozarah <eloise-martin@live.fr>
2018-10-03 15:49:02 +0200

    contenu et stylisation footer

--- view/templates/footer_template.php
+++ view/templates/footer_template.php
@@ -1,6 +1,49 @@
 
 <footer class="container">
     <div>
-        <a href="index.php?action=contact">Contact</a>
+        <p>Developped with <img id="api-logo" src="public/images/api.png" alt=""> API</p>
     </div>
+   
+    <div>
+        <h5>INFOS</h5>
+        <a href="index.php?action=contact">A propos</a>
+        <a href="index.php?action=contact">Contact</a>
+        <a href="index.php?action=contact">Forum</a>
+        <a href="index.php?action=contact"> API</a>
+    </div>
+
+    <div>
+        <h5>COMMUNAUTE</h5>
+        <a href="index.php?action=contact">Facebook</a>
+        <a href="index.php?action=contact">Twitter</a>
+        <a href="index.php?action=contact">Instagram</a>
+    </div>
+
+     <div>
+        <h5>NAVIGATION</h5>
+        <?php 
+            if (isset($_SESSION['userStatus']) && $_SESSION['userStatus'] == 'admin') {
+                echo '<a href="index.php?action=administration">Administration</a>';
+            }
+        ?>
+
+        <a href="index.php">Actus</a>
+        <!-- <a href="index.php?action=weLove">On a aimé</a> -->
+        <a href="index.php?action=tvShows">Séries TV</a>
+
+        <?php
+            if (isset($_SESSION['pseudo'])) {
+        ?>
+                <a href="index.php?action=mySpace">Mon espace</a>
+                <a href="index.php?action=deconnexion">Déconnexion</a>
+        <?php
+            } else {
+                echo '<a href="index.php?action=connexionPage">Connexion</a>';
+            }
+        ?>      
+        <!-- <a href="index.php?action=contact">Contact</a> -->
+    </div>
+
 </footer>
+
+        
