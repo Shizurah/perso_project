8abb3b6031ca95886e9d85e678abfd391cd462f6
Ozarah <eloise-martin@live.fr>
2018-10-11 17:35:11 +0200

    modification menu de l'espace d'administration

--- view/templates/asideForAdministration_template.php
+++ view/templates/asideForAdministration_template.php
@@ -1,12 +1,12 @@
 <aside>
     <div id="administration-aside-content">
         <h2>Gestion du site</h2>
-
-        <a href="index.php?action=administration">Infos site</a> 
+ 
         <a href="index.php?action=allPostsList">Articles</a>
         <a href="index.php?action=postWriting">Rédiger un article</a>
-        <a href="index.php?action=allPostsList">Modifier un article</a>
+        <a href="index.php?action=allPostsListForUpdating">Modifier un article</a>
         <a href="index.php?action=reportedComments">Modération des commentaires</a>
+        <a href="index.php?action=administration">Infos site</a>
         <a href="index.php">Retour au site</a>
         <a href="index.php?action=deconnexion">Déconnexion</a>
     </div>
