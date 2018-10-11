5118e2529864504142b3376fade3be335b5bc39b
Ozarah <eloise-martin@live.fr>
2018-10-11 17:36:11 +0200

    Suppression commentaires

--- view/templates/header_template.php
+++ view/templates/header_template.php
@@ -1,5 +1,5 @@
 <header>
-
+    
     <!-- MENU  -->
     <nav id="nav">
         <?php 
@@ -9,7 +9,6 @@
         ?>
 
         <a href="index.php">Actus</a>
-        <!-- <a href="index.php?action=weLove">On a aimé</a> -->
         <a href="index.php?action=tvShows">Séries TV</a>
 
         <?php
@@ -33,27 +32,6 @@
         <div class="row">
             <h1 class="col-lg-12"><?= $h1Header ?></h1>
         </div>
-
-        <!-- BOUTONS RESEAUX SOCIAUX -->
-        <!-- <div id="actions-btns" class="row">
-
-            <div class="network-logo col-lg-2 col-md-2 col-sm-2 col-xs-2">
-                <a href="#"><img src="public/images/logo_fb1.png" alt="logo Facebook"/></a>
-            </div>
-
-            <div class="network-logo col-lg-2 col-md-2 col-sm-2 col-xs-2">
-                <a href="#"><img src="public/images/logo_twitter1.png" alt="logo Twitter"/></a>
-            </div>
-
-            <div class="network-logo col-lg-2 col-md-2 col-sm-2 col-xs-2">
-                <a href="#"><img src="public/images/logo_instagram1.png" alt="logo Instagram"/></a>
-            </div> 
-        
-            <div id="following-redirection-btn" class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-                <a href="index.php?action=tvShows">Suivre les séries !</a> 
-            </div>
-              
-        </div> -->
         
     </div>   
     
