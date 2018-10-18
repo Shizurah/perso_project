05934bcb7130352a8904d94f6ae6ce5d6ba89e85
Shizurah <eloise-martin@live.fr>
2018-10-18 14:33:34 +0200

    Suppression commentaires

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -3,10 +3,6 @@
 
 abstract class Controller {
 
-    // public function modelAutoloading($class) { // faire une 2e fonction d'autoload ds la classe Controller (parent) pour appel des classes du model ?
-    //     require 'model/' . $class . '.php'; // remplacer 'model' par 'controllers' ?
-    // }
-
     public function errorPage($msg) {
 
         http_response_code(401);
